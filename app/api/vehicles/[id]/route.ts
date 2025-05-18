6b0e8faee53571d54bf016f22fd8b1f30fd91506
Mpho Moipolai <mpho.moipolai@autotrader.co.za>
2025-05-18 16:53:13 +0200

    getting type error

--- app/api/vehicles/[id]/route.ts
+++ app/api/vehicles/[id]/route.ts
@@ -7,7 +7,7 @@ export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const { id } = params
+  const { id } = await params
 
   try {
     const vehicle = await prisma.inventory.findUnique({
