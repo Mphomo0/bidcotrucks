ac8692c50840ce98650ec6f4f363e41cdc000561
Mpho Moipolai <mpho.moipolai@autotrader.co.za>
2025-05-18 14:41:58 +0200

    added layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
 import type { Metadata } from 'next'
-
+import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
+// import Navbar from '@/components/ui/layout/Navbar'
+// import FooterSection from '@/components/ui/layout/FooterSection'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: 'TruckDealer App',
+  title: 'Bidco Trucks',
   description: 'Your Favourite Truck Dealer App',
 }
 
@@ -15,9 +27,15 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <>
-      {children}
-      <ToastContainer />
-    </>
+    <html lang='en'>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* <Navbar /> */}
+        {children}
+        <ToastContainer />
+        {/* <FooterSection /> */}
+      </body>
+    </html>
   )
 }
