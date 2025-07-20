772f354691aa74b3516eeaf18d8b3fe44e4e4da5
Mphomo0 <mphomoipolai1@gmail.com>
2025-07-20 19:50:03 +0200

    removed search/home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 import AboutSection from '@/components/sections/home/AboutSection'
 import Counter from '@/components/sections/home/Counter'
 import Hero from '@/components/sections/home/Hero'
-import HomeSearch from '@/components/search/HomeSearch'
+// import HomeSearch from '@/components/search/HomeSearch'
 import Testimonials from '@/components/sections/home/Testimonials'
 import FeaturedListing from '@/components/sections/home/FeaturedListing'
 
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <HomeSearch />
+      {/* <HomeSearch /> */}
       <AboutSection />
       <Counter />
       <FeaturedListing />
